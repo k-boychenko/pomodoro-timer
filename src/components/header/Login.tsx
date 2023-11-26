ee314ae54e759cd46493d3d40fda6dff0ef2b651
Ksenia <manekinekoster@gmail.com>
2023-11-26 16:49:13 +0900

    User creation for Google/FB sign in

--- src/components/header/Login.tsx
+++ src/components/header/Login.tsx
@@ -1,264 +1,279 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from "react";
 // import GoogleButton from "react-google-button";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import {
-    GoogleAuthProvider,
-    FacebookAuthProvider,
-    signInWithEmailAndPassword,
-    createUserWithEmailAndPassword,
-    signInWithPopup
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
 } from "firebase/auth";
 
 // import components
 import { auth } from "../../firebase/firebase.utils";
 // import { signInWithGoogle } from '../../firebase/firebase.utils';
-import { LogInFormTypes } from '../../common/Const';
-import UsersService from '../../services/UsersService';
-import { IUser } from '../../common/interfaces/users.interface';
+import { LogInFormTypes } from "../../common/Const";
+import UsersService from "../../services/UsersService";
+import { IUser } from "../../common/interfaces/users.interface";
 
 const Login = () => {
-    // useNavigate
-    const navigate = useNavigate();
+  // useNavigate
+  const navigate = useNavigate();
 
-    // useRef
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const passwordConfirmRef = useRef<HTMLInputElement>(null);
+  // useRef
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  const passwordConfirmRef = useRef<HTMLInputElement>(null);
 
-    // useState
-    const [error, setError] = useState("");
-    const [formType, setFormType] = useState(LogInFormTypes.logIn);
+  // useState
+  const [error, setError] = useState("");
+  const [formType, setFormType] = useState(LogInFormTypes.logIn);
 
-    /** Authentication */
-    // Google
-    const googleProvider = new GoogleAuthProvider();
-    const GoogleLogin = async () => {
-        try {
-            const result = await signInWithPopup(auth, googleProvider);
-            console.log(result.user);
-            const user = await UsersService.getUserById(result.user.uid);
-            if (!user) {
-                UsersService.createUser({
-                    uid: result.user.uid,
-                    display_name: result.user.displayName,
-                    email: result.user.email,
-                    last_login: result.user.metadata.lastSignInTime
-                } as IUser);
-            }
+  /** Authentication */
+  // Google
+  const googleProvider = new GoogleAuthProvider();
+  const GoogleLogin = async () => {
+    try {
+      signInWithPopup(auth, googleProvider).then((result) => {
+        UsersService.getUserById(result.user.uid).then((user) => {
+          if (!user.data) {
+            UsersService.createUser({
+              uid: result.user.uid,
+              display_name: result.user.displayName,
+              email: result.user.email,
+              last_login: result.user.metadata.lastSignInTime,
+            } as IUser)
+              .then((res) => {
+                console.log("created a google user", res);
+              })
+              .catch((err) => {
+                console.log("oops, error. ", err);
+              });
+          }
+        });
+      });
 
-            navigate("/");
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    // Facebook
-    const fbProbider = new FacebookAuthProvider();
-    const FacebookLogin = async () => {
-        try {
-            // const result = await signInWithPopup(auth, fbProbider);
-            await signInWithPopup(auth, fbProbider)
-            .then(result => {
-                console.log(result.user.uid);
-                UsersService.getUserById(result.user.uid)
-                .then(user => {
-                    console.log(user);
-                    if (!user) {
-                        UsersService.createUser({
-                            uid: result.user.uid,
-                            display_name: result.user.displayName,
-                            email: result.user.email,
-                            last_login: result.user.metadata.lastSignInTime
-                        } as IUser)
-                        .then(res => { console.log("created a fb user", res); })
-                        .catch(err => { console.log("oops, error. ", err); });
-                    }
-                });
-            });
-            // const credantial = FacebookAuthProvider.credentialFromResult(result);
-            // const token = credantial?.accessToken; 
-            navigate("/");
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    // E-mail & password
-    // TODO
-    const signup = async (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
-        event.preventDefault();
-        
-        try {
-            setError("");
-
-            createUserWithEmailAndPassword(auth, emailRef.current!.value, passwordRef.current!.value)
-            .then((res) => {
-                UsersService.createUser({
-                    uid: res.user.uid,
-                    display_name: "Pomodoro User",
-                    email: res.user.email,
-                    last_login: res.user.metadata.lastSignInTime
-                } as IUser);
-            })
-            .then((res) => {
-                console.log("success ", res);
-                navigate("/");
-            })
-            .catch((err) => {
-                console.log("error ", err);
-            });
-            
-        } catch (error) {
-            setError("Failed to log in");
-        }
-
-    };
-    const login = async (event: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
-        event.preventDefault();
-
-        try {
-            setError("");
-
-            await signInWithEmailAndPassword(auth, emailRef.current!.value, passwordRef.current!.value);
-        } catch (error) {
-            setError("Failed to log in");
-        }
-        
-    };
-
-    // handlers
-    const handleFormTypeChange = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        event.preventDefault();
-
-        if (formType === LogInFormTypes.logIn) {
-            setFormType(LogInFormTypes.signUp);
-        } else {
-            setFormType(LogInFormTypes.logIn);
-        }
+      navigate("/");
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <div className='login-form'>
-            <form>
-                {error && <div>{error}</div>}
-                { (formType === LogInFormTypes.logIn) ? (
-                    <>
-                        <div className='login-input-title'>Email address</div>
-                        <div>
-                            <input 
-                                className='login-email login-inputs-size login-input-border-radius' 
-                                type="email" 
-                                placeholder='Email address'
-                                ref={emailRef}
-                                required
-                            />
-                        </div>
-                        <div className='login-input-title'>Password</div>
-                        <div>
-                            <input 
-                                className='login-password login-inputs-size login-input-border-radius' 
-                                type="password" 
-                                placeholder='Password'
-                                ref={passwordRef}
-                                required
-                            />
-                        </div>
-                        
-                        <div className='login-input-sub'>Forgotten your password?</div>
+  // Facebook
+  const fbProbider = new FacebookAuthProvider();
+  const FacebookLogin = async () => {
+    try {
+      await signInWithPopup(auth, fbProbider).then((result) => {
+        UsersService.getUserById(result.user.uid).then((user) => {
+          if (!user.data) {
+            UsersService.createUser({
+              uid: result.user.uid,
+              display_name: result.user.displayName,
+              email: result.user.email,
+              last_login: result.user.metadata.lastSignInTime,
+            } as IUser)
+              .then((res) => {
+                console.log("created a fb user", res);
+              })
+              .catch((err) => {
+                console.log("oops, error. ", err);
+              });
+          }
+        });
+      });
+      // const credantial = FacebookAuthProvider.credentialFromResult(result);
+      // const token = credantial?.accessToken;
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-                        <div>
-                            <input 
-                                className='login-btn login-btns-size login-border-none login-btn-border-radius' 
-                                type="submit" 
-                                value='Log in'
-                                onClick={login}
-                            />
-                        </div>
-                       
-                        <div className='login-input-sub'>
-                            Don't have an account yet? 
-                            <button className='login-link-btn' onClick={handleFormTypeChange}>
-                                Sign up
-                            </button>
-                        </div>
-                    </>
-                ) : (
-                    <>
-                        <div className='login-input-title'>Email address</div>
-                        <div>
-                            <input 
-                                className='login-email login-inputs-size login-input-border-radius' 
-                                type="email" 
-                                placeholder='Email address'
-                                ref={emailRef}
-                                required
-                            />
-                        </div>
-                        <div className='login-input-title'>Password</div>
-                        <div>
-                            <input 
-                                className='login-password login-inputs-size login-input-border-radius' 
-                                type="password" 
-                                placeholder='Password'
-                                ref={passwordRef}
-                                required
-                            />
-                        </div>
-                        <div>
-                            <input 
-                                className='login-password login-inputs-size login-input-border-radius' 
-                                type="password" 
-                                placeholder='Confirm the password'
-                                ref={passwordConfirmRef}
-                                required
-                            />
-                        </div>
-                        
-                        <div>
-                            <input 
-                                className='login-btn login-btns-size login-border-none login-btn-border-radius' 
-                                type="submit" 
-                                value='Sign up'
-                                onClick={signup}
-                            />
-                        </div>
-                       
-                        <div className='login-input-sub'>
-                            Already have an account? 
-                            <button className='login-link-btn' onClick={handleFormTypeChange}>
-                                Log in
-                            </button>
-                        </div>
-                    </>
-                )}
-            </form>
-            
-            <div className='login-input-title center'>OR</div>
+  // E-mail & password
+  // TODO
+  const signup = async (
+    event: React.MouseEvent<HTMLInputElement, MouseEvent>
+  ) => {
+    event.preventDefault();
 
+    try {
+      setError("");
 
+      createUserWithEmailAndPassword(
+        auth,
+        emailRef.current!.value,
+        passwordRef.current!.value
+      )
+        .then((res) => {
+          UsersService.createUser({
+            uid: res.user.uid,
+            display_name: "Pomodoro User",
+            email: res.user.email,
+            last_login: res.user.metadata.lastSignInTime,
+          } as IUser);
+        })
+        .then((res) => {
+          console.log("success ", res);
+          navigate("/");
+        })
+        .catch((err) => {
+          console.log("error ", err);
+        });
+    } catch (error) {
+      setError("Failed to log in");
+    }
+  };
+  const login = async (
+    event: React.MouseEvent<HTMLInputElement, MouseEvent>
+  ) => {
+    event.preventDefault();
 
-            {/* <GoogleButton onClick={signInWithGoogle}>Sign in with Google</GoogleButton> */}
-            <div className='sns-btns'>
-                <div>
-                    <button 
-                        className='google-btn login-btns-size login-btn-border-radius'
-                        onClick={GoogleLogin}
-                    >
-                            Sign in with Google
-                    </button>
-                </div>
-                <div>
-                    <button 
-                        className='facebook-btn login-btns-size login-border-none login-btn-border-radius'
-                        onClick={FacebookLogin}
-                    >
-                            Sign in with Facebook
-                    </button>
-                </div>
+    try {
+      setError("");
+
+      await signInWithEmailAndPassword(
+        auth,
+        emailRef.current!.value,
+        passwordRef.current!.value
+      );
+    } catch (error) {
+      setError("Failed to log in");
+    }
+  };
+
+  // handlers
+  const handleFormTypeChange = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault();
+
+    if (formType === LogInFormTypes.logIn) {
+      setFormType(LogInFormTypes.signUp);
+    } else {
+      setFormType(LogInFormTypes.logIn);
+    }
+  };
+
+  return (
+    <div className="login-form">
+      <form>
+        {error && <div>{error}</div>}
+        {formType === LogInFormTypes.logIn ? (
+          <>
+            <div className="login-input-title">Email address</div>
+            <div>
+              <input
+                className="login-email login-inputs-size login-input-border-radius"
+                type="email"
+                placeholder="Email address"
+                ref={emailRef}
+                required
+              />
             </div>
+            <div className="login-input-title">Password</div>
+            <div>
+              <input
+                className="login-password login-inputs-size login-input-border-radius"
+                type="password"
+                placeholder="Password"
+                ref={passwordRef}
+                required
+              />
+            </div>
+
+            <div className="login-input-sub">Forgotten your password?</div>
+
+            <div>
+              <input
+                className="login-btn login-btns-size login-border-none login-btn-border-radius"
+                type="submit"
+                value="Log in"
+                onClick={login}
+              />
+            </div>
+
+            <div className="login-input-sub">
+              Don't have an account yet?
+              <button className="login-link-btn" onClick={handleFormTypeChange}>
+                Sign up
+              </button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="login-input-title">Email address</div>
+            <div>
+              <input
+                className="login-email login-inputs-size login-input-border-radius"
+                type="email"
+                placeholder="Email address"
+                ref={emailRef}
+                required
+              />
+            </div>
+            <div className="login-input-title">Password</div>
+            <div>
+              <input
+                className="login-password login-inputs-size login-input-border-radius"
+                type="password"
+                placeholder="Password"
+                ref={passwordRef}
+                required
+              />
+            </div>
+            <div>
+              <input
+                className="login-password login-inputs-size login-input-border-radius"
+                type="password"
+                placeholder="Confirm the password"
+                ref={passwordConfirmRef}
+                required
+              />
+            </div>
+
+            <div>
+              <input
+                className="login-btn login-btns-size login-border-none login-btn-border-radius"
+                type="submit"
+                value="Sign up"
+                onClick={signup}
+              />
+            </div>
+
+            <div className="login-input-sub">
+              Already have an account?
+              <button className="login-link-btn" onClick={handleFormTypeChange}>
+                Log in
+              </button>
+            </div>
+          </>
+        )}
+      </form>
+
+      <div className="login-input-title center">OR</div>
+
+      {/* <GoogleButton onClick={signInWithGoogle}>Sign in with Google</GoogleButton> */}
+      <div className="sns-btns">
+        <div>
+          <button
+            className="google-btn login-btns-size login-btn-border-radius"
+            onClick={GoogleLogin}
+          >
+            Sign in with Google
+          </button>
         </div>
-    );
+        <div>
+          <button
+            className="facebook-btn login-btns-size login-border-none login-btn-border-radius"
+            onClick={FacebookLogin}
+          >
+            Sign in with Facebook
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/server/controller/users.controller.js
+++ src/server/controller/users.controller.js
@@ -4,125 +4,115 @@ const User = db.users;
 
 // Create and Save a new User
 exports.create = (req, res) => {
-    // validate
-    console.log(req.body);
-    if (!req.body.uid) {
-        res.status(400).send({
-            message: "Content cannot be empty!"
-        });
-        return;
-    }
-    // create
-    const user = {
-        user_id: req.body.uid,
-        display_name: req.body.display_name,
-        email: req.body.email,
-        last_login: req.body.last_login,
-    };
-    User.create(user)
-    .then(data => {
-        res.send(data);
+  // validate
+  if (!req.body.uid) {
+    res.status(400).send({
+      message: "Content cannot be empty!",
+    });
+    return;
+  }
+  // create
+  const user = {
+    user_id: req.body.uid,
+    display_name: req.body.display_name,
+    email: req.body.email,
+    last_login: req.body.last_login,
+  };
+  User.create(user)
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err => {
-        res.status(500).send({
-            message:
-            err.message || "Some error occured while retrieving users."
-        });
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occured while retrieving users.",
+      });
     });
 };
 
 // Retrieve all Users from the database.
 exports.findAll = (req, res) => {
-    // const title = req.query.title;
-    // var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
-    User.findAll()
-    .then(data => {
-        res.send(data);
+  // const title = req.query.title;
+  // var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  User.findAll()
+    .then((data) => {
+      res.send(data);
     })
-    .catch(err => {
-        res.status(500).send({
-            message:
-            err.message || "Some error occurred while retrieving Users."
-        });
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving Users.",
+      });
     });
 };
 
 // Find a single User with an id
 exports.findOne = (req, res) => {
-    console.log(req.body);
-    console.log("uid: " + req.body.uid);
-  User.findOne(req.body.uid)
-  .then(data => {
-    res.send(data);
-  })
-  .catch(err => {
-    res.status(500).send({
-        message:
-        err.message || "Some error occurred while retrieving Users."
+  User.findOne({ where: { user_id: req.params.uid } })
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while retrieving Users.",
+      });
     });
-  })
 };
 
 // Update a User by the id in the request
 exports.update = (req, res) => {
-    // validate
-    if (!req.body.uid) {
-        res.status(400).send({
-            message: "Content cannot be empty!"
+  // validate
+  if (!req.params.uid) {
+    res.status(400).send({
+      message: "Content cannot be empty!",
+    });
+    return;
+  }
+  // update
+  User.update(req.body, {
+    where: { user_id: req.params.uid }, // ??? check the source
+  })
+    .then((num) => {
+      if (num === 1) {
+        res.send({
+          message: "User was successfully updated.",
         });
-        return;
-    }
-    // update
-    User.update(req.body, {
-        where: {user_id: req.params.uid} // ??? check the source
-    })
-    .then(num => {
-        if (num === 1) {
-            res.send({
-                message: 'User was successfully updated.'
-            });
-        } else {
-            res.send({
-                message: `Couldn't update the User.`
-            });
-        }
-    })
-    .catch(err => {
-        res.status(500).send({
-            message:
-            err.message || "Error occured while updating the User"
+      } else {
+        res.send({
+          message: `Couldn't update the User.`,
         });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error occured while updating the User",
+      });
     });
 };
 
 // Delete a User with the specified id in the request
 exports.delete = (req, res) => {
-    // delete
-    User.destroy({
-        where: {user_id: req.params.uid}
-    })
-    .then(num => {
-        if (num === 1) {
-            res.send({
-                message: 'User was successfully deleted.',
-                result: true
-            });
-        } else {
-            res.send({
-                message: `Couldn't delete the User.`,
-                result: false
-            });
-        }
-    })
-    .catch(err => {
-        res.status(500).send({
-            message:
-            err.message || "Error occured while deleting the User"
+  // delete
+  User.destroy({
+    where: { user_id: req.params.uid },
+  })
+    .then((num) => {
+      if (num === 1) {
+        res.send({
+          message: "User was successfully deleted.",
+          result: true,
         });
+      } else {
+        res.send({
+          message: `Couldn't delete the User.`,
+          result: false,
+        });
+      }
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: err.message || "Error occured while deleting the User",
+      });
     });
 };
 
 // Delete all Users from the database.
-exports.deleteAll = (req, res) => {
-  
-};
+exports.deleteAll = (req, res) => {};
